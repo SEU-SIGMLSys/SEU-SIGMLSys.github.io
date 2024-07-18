6741ee50c43432d89241ab7a85d5463e78237cf3
SEU-SIGMLSys <hkruff02@gmail.com>
2024-07-19 01:29:58 +0800

    Update index.html, schedules.js and slides

--- schedules.js
+++ schedules.js
@@ -1,1 +1,90 @@
-var schedules = [{"title":"Accelerating Neural Recommendation Training with Embedding Scheduling","conf":"NSDI'24","presenter":"Kai Huang","date":"July 4, 2024.","time":"7:00 p.m.","location":"Room CSxxx @ SEU & Online","links":[{"title":"TencentMeeting","url":"https://meeting.tencent.com/dm/cGqoEdimqm2W"},{"title":"Slides","url":"/slides/2024-7-4.pdf"}],"dblp":{"source":"DBLP","url":"https://www.usenix.org/conference/nsdi24/presentation/zeng"}},{"title":"Vulcan: Automatic Query Planning for Live ML Analytics","conf":"NSDI'24","presenter":"Minyuan Feng","date":"July 11, 2024.","time":"7:00 p.m.","location":"Room CSxxx @ SEU & Online","links":[{"title":"TencentMeeting","url":"https://meeting.tencent.com/dm/1ytQw5VuAPv0"},{"title":"Slides","url":"/slides/2024-7-11-1.pdf"}],"dblp":{"source":"DBLP","url":"https://www.usenix.org/conference/nsdi24/presentation/zhang-yiwen"}},{"title":"MadEye: Boosting Live Video Analytics Accuracy with Adaptive Camera Configurations","conf":"NSDI'24","presenter":"Yujie Chen","date":"July 11, 2024.","time":"8:00 p.m.","location":"Room CSxxx @ SEU & Online","links":[{"title":"TencentMeeting","url":"https://meeting.tencent.com/dm/1ytQw5VuAPv0"},{"title":"Slides","url":"/slides/2024-7-11-2.pdf"}],"dblp":{"source":"DBLP","url":"https://www.usenix.org/conference/nsdi24/presentation/wong"}}]
+var schedules = [
+    {
+        "title":"CASSINI: Network-Aware Job Scheduling in Machine Learning Clusters",
+        "conf":"NSDI'24",
+        "presenter":"Botai Sun",
+        "date":"July 18, 2024.",
+        "time":"7:00 p.m.",
+        "location":"Room CSxxx @ SEU & Online",
+        "links":[
+            {
+                "title":"TencentMeeting",
+                "url":"https://meeting.tencent.com/dm/RfNf0JCHCja7"
+            },
+            {
+                "title":"Slides",
+                "url":"/slides/2024-7-18.pdf"
+            }
+        ],
+        "dblp":{
+            "source":"DBLP",
+            "url":"https://www.usenix.org/conference/nsdi24/presentation/rajasekaran"
+        }
+    },
+    {
+        "title":"MadEye: Boosting Live Video Analytics Accuracy with Adaptive Camera Configurations",
+        "conf":"NSDI'24",
+        "presenter":"Yujie Chen",
+        "date":"July 11, 2024.",
+        "time":"8:00 p.m.",
+        "location":"Room CSxxx @ SEU & Online",
+        "links":[
+            {
+                "title":"TencentMeeting",
+                "url":"https://meeting.tencent.com/dm/1ytQw5VuAPv0"
+            },
+            {
+                "title":"Slides",
+                "url":"/slides/2024-7-11-2.pdf"
+            }
+            ],
+        "dblp":{
+            "source":"DBLP",
+            "url":"https://www.usenix.org/conference/nsdi24/presentation/wong"
+        }
+    },
+    {
+        "title":"Vulcan: Automatic Query Planning for Live ML Analytics",
+        "conf":"NSDI'24",
+        "presenter":"Minyuan Feng",
+        "date":"July 11, 2024.",
+        "time":"7:00 p.m.",
+        "location":"Room CSxxx @ SEU & Online",
+        "links":[
+            {
+                "title":"TencentMeeting",
+                "url":"https://meeting.tencent.com/dm/1ytQw5VuAPv0"
+            },
+            {
+                "title":"Slides",
+                "url":"/slides/2024-7-11-1.pdf"
+            }
+        ],
+        "dblp":{
+            "source":"DBLP",
+            "url":"https://www.usenix.org/conference/nsdi24/presentation/zhang-yiwen"
+        }
+    },
+    {
+        "title":"Accelerating Neural Recommendation Training with Embedding Scheduling",
+        "conf":"NSDI'24",
+        "presenter":"Kai Huang",
+        "date":"July 4, 2024.",
+        "time":"7:00 p.m.",
+        "location":"Room CSxxx @ SEU & Online",
+        "links":[
+            {
+                "title":"TencentMeeting",
+                "url":"https://meeting.tencent.com/dm/cGqoEdimqm2W"
+            },
+            {
+                "title":"Slides",
+                "url":"/slides/2024-7-4.pdf"
+            }
+        ],
+        "dblp":{
+            "source":"DBLP",
+            "url":"https://www.usenix.org/conference/nsdi24/presentation/zeng"
+        }
+    },
+    ]
