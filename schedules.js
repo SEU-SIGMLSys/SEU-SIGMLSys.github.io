70eebfcf5a35d176f99fce0833bf1a9b9e475780
hk <220242329@seu.edu.cn>
2024-11-15 00:18:26 +0800

    2024/11/15 schedules

--- schedules.js
+++ schedules.js
@@ -10,7 +10,7 @@ var schedules = [
         "links":[
             {
                 "title":"TencentMeeting",
-                "url":"https://meeting.tencent.com/dm/5y7V5wQ9vMyI"
+                "url":"https://meeting.tencent.com/dm/yehYWE3IKQaM"
             },
             {
                 "title":"Slides",
@@ -37,7 +37,7 @@ var schedules = [
         "links":[
             {
                 "title":"TencentMeeting",
-                "url":"https://meeting.tencent.com/dm/5y7V5wQ9vMyI"
+                "url":"https://meeting.tencent.com/dm/yehYWE3IKQaM"
             },
             {
                 "title":"Slides",
